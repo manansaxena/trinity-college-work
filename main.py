cc547e3769885d3bd6683612e697aeac7edef4f5
Manan Saxena <32758106+manansaxena@users.noreply.github.com>
2024-05-01 19:02:13 +0000

    requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-os.system("git clone https://github.com/hukkelas/DSFD-Pytorch-Inference.git")
-os.system("python3 DSFD-Pytorch-Inference/setup.py install")
+# import os
+# os.system("git clone https://github.com/hukkelas/DSFD-Pytorch-Inference.git")
+# os.system("python3 DSFD-Pytorch-Inference/setup.py install")
 
 import streamlit as st 
 import cv2
